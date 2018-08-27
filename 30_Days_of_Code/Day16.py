9c0f36cc50c59db50f0c5134d9522283760bc22a
Ryan Yao <ryanyao@socle-tech.com>
2018-08-27 13:46:26 +0800

    1.  Add 30_Days_of_Code/Day16.py, Day17.py

---
+++ 30_Days_of_Code/Day16.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+try:
+    val = int(input())
+    print(val)
+except:
+    print("Bad String")
---
+++ 30_Days_of_Code/Day17.py
@@ -0,0 +1,22 @@
+#Write your code here
+import math
+
+class Calculator(Exception):
+    def __init__(self):
+        pass
+    
+    def power(self, n, p):
+        if(n>=0 and p>=0):
+            return int(math.pow(n, p))
+        else:
+            raise Exception("n and p should be non-negative")
+
+myCalculator=Calculator()
+T=int(input())
+for i in range(T):
+    n,p = map(int, input().split())
+    try:
+        ans=myCalculator.power(n,p)
+        print(ans)
+    except Exception as e:
+        print(e) 
